8df85d2e1b66ec7a5060400ab0e842fec3b7ff79
Oliver-taken2024 <oliver.w.pettersson@outlook.com>
2026-02-03 09:49:05 +0100

    Add project files.

---
+++ Poke_Repo/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Poke_Repo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
