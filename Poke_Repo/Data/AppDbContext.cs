09993c48c40209bdf49c5eb798bbb4170365a7fe
Oliver-taken2024 <oliver.w.pettersson@outlook.com>
2026-02-03 10:15:55 +0100

    Started

---
+++ Poke_Repo/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Poke_Repo.Model;
+
+namespace Poke_Repo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+
+        }
+      public DbSet<PokemonModel> Pokemons { get; set; }
+    }
+
+    
+}
